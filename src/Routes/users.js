a133e3a09a6286245e9f2ecc4f8ad888a0c4dce0
Rodrigo Choucino de Soto <rodrigodesoto@hotmail.com>
2022-03-17 23:38:14 -0300

    Funcoes async

--- src/Routes/users.js
+++ src/Routes/users.js
@@ -3,46 +3,49 @@ const router = express.Router();
 const Users = require('../model/user');
 const bcrypt = require('bcrypt');
 
-router.get('/', (req, res) => {
-    Users.find({}, (err, data) => {
-        if (err) return res.send({ error: 'Erro na consulta de usuários'});
-        return res.send(data);
-    });
+router.get('/', async (req, res) => {
+    try{
+        const users = await Users.find({});
+        return res.send(users);
+    }catch(err){
+        return res.status(500).send({error: 'Erro na consulta do usuário!'});
+    }
 });
 
-router.post('/create', (req, res) => {
+router.post('/create', async (req, res) => {
     const {email, password} = req.body;
-
     if(!email || !password) return res.status(400).send({ error: 'Dados insuficientes!' });
 
-        Users.findOne({email}, (err, data) => {
-            if(err) return res.status(400).send({ error: 'Erro ao buscar usuário!'});
-            if(data) return res.status(400).send({ error: 'Usuário já registrado!'});
+    try{
+        if (await Users.findOne({email})) return res.status(400).send({ error: 'Usuário já registrado!'});
+        await Users.create(req.body, (err, data) => {
+            if(err) return res.status(400).send({ error: 'Erro ao criar usuário!'});
 
-            Users.create(req.body, (err, data) => {
-                if(err) return res.status(400).send({ error: 'Erro ao criar usuário!'});
-
-                data.password = undefined;
-                return res.status(201).send(data);
-            });
-        });
-});
-
-router.post('/auth', (req, res) => {
-    const {email, password} = req.body;
-
-    if(!email || !password) return res.status(400).send({ error: 'Dados insuficientes!' });
-
-    Users.findOne({email}, (err, data) => {
-        if(err) return res.status(400).send({ error: 'Erro ao buscar usuário!'});
-        if(!data) return res.status(400).send({ error: 'Usuário não registrado!'});
-
-        bcrypt.compare(password, data.password, (err, same) =>{
-            if(!same) return res.status(400).send({ error: 'Erro ao autenticar o usuário!'});
             data.password = undefined;
-            return res.send(data);
+            return res.status(201).send(data);
         });
-    }).select('+password');
+    }catch(err){
+        return res.status(500).send({ error: 'Erro ao buscar usuário!'});
+    }
+});
+
+router.post('/auth', async (req, res) => {
+    const {email, password} = req.body;
+
+    if(!email || !password) return res.status(400).send({ error: 'Dados insuficientes!' });
+
+    try{
+        const user = await Users.findOne({email}).select('+password');
+        if(!user) return res.status(400).send({ error: 'Usuário não registrado!'});
+        const pass_ok = await bcrypt.compare(password, user.password);
+
+        if(!pass_ok) return res.status(401).send({ error: 'Erro ao autenticar o usuário!'});  
+
+        user.password = undefined;
+        return res.send({user});
+    }catch(err){
+        return res.status(500).send({ error: 'Erro ao buscar usuário!'});
+    }
 });
 
 module.exports = router;
